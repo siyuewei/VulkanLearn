7cb1e5703ac4428cecfa9824a7927b770230ccbd
危思越 <weisiyue071024@sjtu.edu.cn>
2025-12-24 19:50:47 +0800

    把同步信号量的个数提升到跟image相同的个数。CPU的渲染速度大于显示器显示速度这个问题，由抢信号量提升到抢image。

--- VulkanLearnApplication.cpp
+++ VulkanLearnApplication.cpp
@@ -40,7 +40,7 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
-const int MAX_FRAMES_IN_FLIGHT = 2;
+const int MAX_FRAMES_IN_FLIGHT = 3;
 
 struct Vertex
 {
